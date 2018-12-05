b0517184294e6e515c859ca0205eb620eedcae75
Samoski93 <samoski93@hotmail.com>
2018-12-05 16:42:07 +0100

    Added seed data

--- DatingAPP.API/Startup.cs
+++ DatingAPP.API/Startup.cs
@@ -116,7 +116,7 @@ namespace DatingAPP.API
             }
 
             // app.UseHttpsRedirection();
-            // seeder.SeedUsers();
+            seeder.SeedUsers();
             app.UseCors(x => x.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             app.UseAuthentication();
             app.UseDefaultFiles();
